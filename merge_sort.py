6a71c63bdb11133039ac4b0f275467b52596daed
unknown <anjalo-200>
2025-12-15 00:27:24 +0530

    Merge sort file

---
+++ merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    return arr
+list1 = [7.1,5,98,-1,-9,99,34,3,0]
+print("original list :", list1)
+print("sorted list:", merge_sort(list1))
