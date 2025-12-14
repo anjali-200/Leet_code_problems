8841920445a79f245d36600c7c26716ebbb8feef
unknown <anjalo-200>
2025-12-15 01:30:05 +0530

    python program for palindrome leetcode

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+        reversedHalf = 0
+        while x > reversedHalf:
+            reversedHalf = reversedHalf * 10 + x % 10
+            x //= 10
+        return x == reversedHalf or x == reversedHalf // 10
+
+solution = Solution()
+print(solution.isPalindrome(121))  # True
+print(solution.isPalindrome(-121))  # False
+print(solution.isPalindrome(10))    # False
